8d027c85f5516917e14d18edf02cee5407b9af61
httpsmenahassan <whobutmenacodes@gmail.com>
2023-05-14 14:30:50 -0400

    updated groceryHaul table to include functional add item button which adds table rows that mirrors the rows that populate from groceryHaul.ejs -- also update delete button within that same table to be functional

--- public/js/groceries.js
+++ public/js/groceries.js
@@ -59,15 +59,76 @@ const foodAddSubmitBtn = document.querySelector('.foodAddSubmit')
 // })
 
 
-const file = document.querySelector('.imgInput')
-foodAddSubmitBtn.disabled = true
+// const file = document.querySelector('.imgInput')
+// foodAddSubmitBtn.disabled = true
 
-file.addEventListener('change', (e) => {
-  const isEmpty = e.target.value.length == 0
-  console.log(isEmpty)
-  if(isEmpty){
-    foodAddSubmitBtn.disabled = true
-  } else {
-    foodAddSubmitBtn.disabled = false
+// file.addEventListener('change', (e) => {
+//   const isEmpty = e.target.value.length == 0
+//   console.log(isEmpty)
+//   if(isEmpty){
+//     foodAddSubmitBtn.disabled = true
+//   } else {
+//     foodAddSubmitBtn.disabled = false
+//   }
+// })
+
+// delete table rows that were created as a result of Azure
+document.addEventListener("DOMContentLoaded", () => {
+  const deleteButtons = document.querySelectorAll(".tableRowDelete");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const row = event.target.parentNode.parentNode;
+      row.remove();
+    });
+  });
+});
+
+
+// this code is to add new table rows when the Add Item button is clicked
+const addItemBtn = document.querySelector('#addItemBtn');
+const tBody = document.querySelector('#tBody');
+
+addItemBtn.addEventListener('click', () => {
+  const newRow = document.createElement('tr');
+  const today = new Date();
+
+  const expirationDate = new Date();
+  expirationDate.setTime(expirationDate.getTime() + 1000 * 60 * 60 * 24 * 4);
+  const formattedExpirationDate = expirationDate.toLocaleDateString('en-US', {
+    month: '2-digit',
+    day: '2-digit', 
+    year: 'numeric'
+  });
+  const formattedDate = today.toLocaleDateString('en-US', {
+    month: '2-digit', day: '2-digit', year: 'numeric'
+  });
+  for (let i = 0; i < 4; i++) {
+    const cell = document.createElement('td');
+    const input = document.createElement('input');
+    const names = ['food', 'purchaseDate', 'quantity', 'expirationDate'];
+    input.name = names[i];
+    if (i === 1) {
+      // prefill second input with today's date
+      
+      input.value = formattedDate;
+    }  else if (i === 2) {
+      // prefill third input with a quantity number of 1
+      input.value = 1;
+    } else if (i === 3) {
+      // prefill fourth input with a date four days from now
+      input.value = formattedExpirationDate;
+    }
+    cell.appendChild(input);
+    newRow.appendChild(cell);
   }
-})
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete item';
+  deleteBtn.addEventListener('click', () => {
+    newRow.remove();
+  });
+  const deleteTd = document.createElement('td');
+  deleteTd.appendChild(deleteBtn);
+  newRow.appendChild(deleteTd);
+  tBody.appendChild(newRow);
+});
